0486dcb023579f2a3b99fe8f8f6fc263c4d9ff44
lselbeck <lselbeck@gmail.com>
2017-02-22 17:03:20 -0800

    Working on update interval

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -11,25 +11,66 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        //control variables
         this.BOARD_SIZE = 20; //the number of squares tall the board is
-        this.viewportW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-        this.viewportH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-        this.boardH = this.BOARD_SIZE;
-        this.boardW = Math.floor(this.boardH * this.viewportW / this.viewportH);
-        this.squareH = Math.floor(this.viewportH * 0.95 / this.boardH);
-        this.squareW = Math.floor(this.viewportW * 0.95 / this.boardW);
+        this.BOARD_INTERVAL = 1000; //number of milliseconds the board takes to update
+        this.initBoard();
+        this.boardUpdater = setInterval(this.updateBoard(), this.BOARD_INTERVAL);
+    }
+    AppComponent.prototype.initBoard = function () {
+        var viewportW = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        var viewportH = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        var boardH = this.BOARD_SIZE;
+        var boardW = Math.floor(boardH * viewportW / viewportH);
+        this.squareH = Math.floor(viewportH * 0.95 / boardH);
+        this.squareW = Math.floor(viewportW * 0.95 / boardW);
         this.board = [];
-        for (var i = 0; i < this.boardH; i++) {
+        for (var i = 0; i < boardH; i++) {
             this.board[i] = [];
-            for (var j = 0; j < this.boardW; j++) {
+            for (var j = 0; j < boardW; j++) {
                 this.board[i][j] = false;
             }
         }
-    }
+    };
     AppComponent.prototype.flipSquare = function (y, x) {
         this.board[y][x] = !this.board[y][x];
     };
+    //Apply conway's rules every update
     AppComponent.prototype.updateBoard = function () {
+        for (var y = 0; y < this.board.length; y++) {
+            for (var x = 0; x < this.board[0].length; x++) {
+                var numberOfNeighbors = this.getNumberOfNeighbors(y, x);
+                if (this.board[y][x] && numberOfNeighbors < 2)
+                    this.board[y][x] = false;
+                else if (this.board[y][x] && (numberOfNeighbors == 2 || numberOfNeighbors == 3))
+                    this.board[y][x] = true;
+                else if (this.board[y][x] && numberOfNeighbors > 3)
+                    this.board[y][x] = false;
+                else if (!this.board[y][x] && numberOfNeighbors == 3)
+                    this.board[y][x] = true;
+            }
+        }
+    };
+    AppComponent.prototype.getNumberOfNeighbors = function (y, x) {
+        var count = 0;
+        if (this.board[Math.max(y - 1, 0)][Math.max(x - 1, 0)])
+            count++;
+        if (this.board[Math.max(y - 1, 0)][x])
+            count++;
+        if (this.board[Math.max(y - 1, 0)][Math.min(x + 1, this.board[x].length - 1)])
+            count++;
+        if (this.board[y][Math.min(x + 1, this.board[x].length - 1)])
+            count++;
+        if (this.board[Math.min(y + 1, this.board.length - 1)][Math.min(x + 1, this.board[x].length - 1)])
+            count++;
+        if (this.board[Math.min(y + 1, this.board.length - 1)][x])
+            count++;
+        if (this.board[Math.min(y + 1, this.board.length - 1)][Math.max(x - 1, 0)])
+            count++;
+        if (this.board[y][Math.max(x - 1, 0)])
+            ;
+        count++;
+        return count;
     };
     AppComponent = __decorate([
         core_1.Component({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,22 +17,33 @@ import { Component } from '@angular/core';
 	`,
 })
 export class AppComponent { 
-	BOARD_SIZE: number = 20 //the number of squares tall the board is
+	//control variables
+	BOARD_SIZE: number = 20; //the number of squares tall the board is
+	BOARD_INTERVAL: number = 1000; //number of milliseconds the board takes to update
 
-	viewportW: number = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
-	viewportH: number = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
-	boardH: number = this.BOARD_SIZE;
-	boardW: number = Math.floor(this.boardH * this.viewportW / this.viewportH);
-	squareH: number = Math.floor(this.viewportH * 0.95 / this.boardH);
-	squareW: number = Math.floor(this.viewportW * 0.95 / this.boardW);
+	//board stuff
+	squareH: number;
+	squareW: number;
 	board: boolean[][];
-	
+	boardUpdater: any;
 
 	constructor() {
+		this.initBoard();
+		this.boardUpdater = setInterval(this.updateBoard(), this.BOARD_INTERVAL);
+	}
+
+	initBoard() {
+		let viewportW: number = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		let viewportH: number = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		let boardH: number = this.BOARD_SIZE;
+		let boardW: number = Math.floor(boardH * viewportW / viewportH);
+		this.squareH = Math.floor(viewportH * 0.95 / boardH);
+		this.squareW = Math.floor(viewportW * 0.95 / boardW);
+
 		this.board = [];
-		for (let i = 0; i < this.boardH; i++) {
+		for (let i = 0; i < boardH; i++) {
 			this.board[i] = [];
-			for (let j = 0; j < this.boardW; j++) {
+			for (let j = 0; j < boardW; j++) {
 				this.board[i][j] = false;
 			}
 		}
@@ -42,7 +53,29 @@ export class AppComponent {
 		this.board[y][x] = !this.board[y][x];
 	}
 
+	//Apply conway's rules every update
 	updateBoard() {
-		
+		for (let y = 0; y < this.board.length; y++) {
+			for (let x = 0; x < this.board[0].length; x++) {
+				let numberOfNeighbors = this.getNumberOfNeighbors(y, x);
+				if (this.board[y][x] && numberOfNeighbors < 2) this.board[y][x] = false;
+				else if (this.board[y][x] && (numberOfNeighbors == 2 || numberOfNeighbors == 3)) this.board[y][x] = true;
+				else if (this.board[y][x] && numberOfNeighbors > 3) this.board[y][x] = false;
+				else if (!this.board[y][x] && numberOfNeighbors == 3) this.board[y][x] = true;
+			}
+		}
+	}
+
+	getNumberOfNeighbors(y: number, x: number): number {
+		let count: number = 0;
+		if (this.board[Math.max(y-1, 0)][Math.max(x-1, 0)]) count++;
+		if (this.board[Math.max(y-1, 0)][x]) count++;
+		if (this.board[Math.max(y-1, 0)][Math.min(x+1, this.board[x].length-1)]) count++;
+		if (this.board[y][Math.min(x+1, this.board[x].length-1)]) count++;
+		if (this.board[Math.min(y+1, this.board.length-1)][Math.min(x+1, this.board[x].length-1)]) count++;
+		if (this.board[Math.min(y+1, this.board.length-1)][x]) count++;
+		if (this.board[Math.min(y+1, this.board.length-1)][Math.max(x-1, 0)]) count++;
+		if (this.board[y][Math.max(x-1, 0)]]) count++;
+		return count;
 	}
 }
